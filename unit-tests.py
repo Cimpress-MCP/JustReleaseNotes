dadcb30cdd46661ba070bdad9e0e5979fc0f0e34
Rafal Nowosielski <rafalnowosielski@me.com>
2015-04-27 00:32:12 +0200

    Trying to fix the build job setup.

--- unit-tests.py
+++ unit-tests.py
@@ -4,6 +4,8 @@ import JustReleaseNotes
 
 if __name__ == '__main__':
     test_loader = unittest.defaultTestLoader
-    test_runner = unittest.TextTestRunner()
     test_suite = test_loader.discover("tests", pattern='*_Test.py')
-    test_runner.run(test_suite)
+    res = unittest.TestResult()
+    test_suite.run(res)
+    if not res.wasSuccessful():
+        sys.exit(1)
