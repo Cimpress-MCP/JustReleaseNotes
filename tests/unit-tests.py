ea8f79f461f7932ddcd1715e3cdd0d21b20b9889
Rafal Nowosielski <rafalnowosielski@me.com>
2015-04-26 23:15:39 +0200

    Trying to fix the build job setup.

--- tests/unit-tests.py
+++ tests/unit-tests.py
@@ -1,8 +1,11 @@
 import unittest
+import sys
 
 if __name__ == '__main__':
-    test_loader = unittest.defaultTestLoader
-    test_runner = unittest.TextTestRunner()
-    test_suite = test_loader.discover("tests", pattern='*_Test.py')
-    test_runner.run(test_suite)
-
+    try:
+        test_loader = unittest.defaultTestLoader
+        test_runner = unittest.TextTestRunner()
+        test_suite = test_loader.discover("tests", pattern='*_Test.py')
+        test_runner.run(test_suite)
+    except:
+        sys.exit(1)
