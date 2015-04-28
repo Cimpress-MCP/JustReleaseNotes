a704b7b3b9544976bce755e2f73568718ca7e478
Rafal Nowosielski <rnowosielski@cimpress.com>
2015-04-28 18:07:19 +0200

    Uncomment the error handling in factory

--- JustReleaseNotes/sourcers/factory.py
+++ JustReleaseNotes/sourcers/factory.py
@@ -1,9 +1,9 @@
 import importlib
 
 def create(conf):
-    #try:
+    try:
         module = importlib.import_module("JustReleaseNotes.sourcers.{0}".format(conf["Provider"]))
         issuerClass = getattr(module, conf["Provider"])
         return issuerClass(conf)
-    #except:
-     #   raise Exception("Source code repository is needed to retrieve tags: {0} not found".format(conf["Provider"]))
+    except:
+        raise Exception("Source code repository is needed to retrieve tags: {0} not found".format(conf["Provider"]))
