890d9494212da6f41f2b45da2de08787ec47b42a
Rafal Nowosielski <rnowosielski@cimpress.com>
2015-08-26 17:36:21 +0200

    Fixing test for command_line.py #5

--- tests/command_line_Test.py
+++ tests/command_line_Test.py
@@ -13,6 +13,25 @@ else:
 
 class ReleaseNote_Test(unittest.TestCase):
 
+    mockedData = '{ '\
+                      '"pathToSave": "..", ' \
+                      '"packages": { ' \
+                      '     "JustReleaseNotes": { ' \
+                      '     "Issues": { ' \
+                      '         "Provider": "TestProvider",     ' \
+                      '         "HtmlUrl": "https://Just.Release.Notes/issues", ' \
+                      '         "Url": "https://Just.Release.Notes/issues" }, ' \
+                      '     "Source": { ' \
+                      '         "Provider": "TestRepo", ' \
+                      '         "RepositoryUrl": ' \
+                      '         "https://Just.Release.Notes/repo.git" ' \
+                      '     }, ' \
+                      '     "ReleaseNotesWriter": "TestWriter" ' \
+                      '   }' \
+                      ' } ' \
+                      '}'
+
+
     @patch("JustReleaseNotes.issuers.factory")
     @patch("JustReleaseNotes.sourcers.factory")
     @patch("JustReleaseNotes.writers.factory")
@@ -24,70 +43,27 @@ class ReleaseNote_Test(unittest.TestCase):
         writer_mock = MagicMock()
         writers_factory.create.return_value = writer_mock
         writer_mock.getExtension.return_value = ".ext"
-
-        mocked_open = mock_open(
-                read_data='{ '
-                          '"pathToSave": "..", '
-                          '"packages": { '
-                          '     "JustReleaseNotes": { '
-                          '     "Issues": { '
-                          '         "Provider": "TestProvider",     '
-                          '         "HtmlUrl": "https://Just.Release.Notes/issues", '
-                          '         "Url": "https://Just.Release.Notes/issues" }, '
-                          '     "Source": { '
-                          '         "Provider": "TestRepo", '
-                          '         "RepositoryUrl": '
-                          '         "https://Just.Release.Notes/repo.git" '
-                          '     }, '
-                          '     "ReleaseNotesWriter": "TestWriter" '
-                          '   }'
-                          ' } '
-                          '}')
-
+        mocked_open = mock_open(read_data=self.mockedData)
         with patch.object(builtins, 'open', mocked_open):
             JustReleaseNotes.command_line.generate_release_notes("config.json")
 
         mocked_open.assert_called_with(os.getcwd() + os.sep + ".." + os.sep + "JustReleaseNotes" + os.sep + "index.ext", "wb")
         mocked_open().write.assert_called_once_with(b'')
 
-    @patch("sys.exit")
-    def test_main_WithoutParams_DoesNothing(self, sysExit):
-        JustReleaseNotes.command_line.main([])
-        sysExit.assert_called_once_with(2)
-
     @patch("JustReleaseNotes.issuers.factory")
     @patch("JustReleaseNotes.sourcers.factory")
     @patch("JustReleaseNotes.writers.factory")
     @patch("os.path.exists")
     @patch("os.makedirs")
-    def test_justRelesdeNotes_withDefaultConfig_NoData_DoesNotRaise_ProducesEmptyOutput(self, issuers_factory, repo_factory,
+    def test_main_withDefaultConfig_NoData_DoesNotRaise_ProducesEmptyOutput(self, issuers_factory, repo_factory,
         writers_factory, path_exists, path_makedirs):
 
         writer_mock = MagicMock()
         writers_factory.create.return_value = writer_mock
         writer_mock.getExtension.return_value = ".ext"
-
-        mocked_open = mock_open(
-                read_data='{ '
-                          '"pathToSave": "..", '
-                          '"packages": { '
-                          '     "JustReleaseNotes": { '
-                          '     "Issues": { '
-                          '         "Provider": "TestProvider",     '
-                          '         "HtmlUrl": "https://Just.Release.Notes/issues", '
-                          '         "Url": "https://Just.Release.Notes/issues" }, '
-                          '     "Source": { '
-                          '         "Provider": "TestRepo", '
-                          '         "RepositoryUrl": '
-                          '         "https://Just.Release.Notes/repo.git" '
-                          '     }, '
-                          '     "ReleaseNotesWriter": "TestWriter" '
-                          '   }'
-                          ' } '
-                          '}')
-
+        mocked_open = mock_open(read_data=self.mockedData)
         with patch.object(builtins, 'open', mocked_open):
-            JustReleaseNotes.command_line.main(["notes", "-c", "config.json"])
+            JustReleaseNotes.command_line.main([])
 
         mocked_open.assert_called_with(os.getcwd() + os.sep + ".." + os.sep + "JustReleaseNotes" + os.sep + "index.ext", "wb")
         mocked_open().write.assert_called_once_with(b'')
