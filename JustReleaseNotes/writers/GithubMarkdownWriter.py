7ee636971ebe4b45dffe02562589d5fa5312f463
Rafał Nowosielski <rafal@nowosielski.link>
2015-09-26 17:15:12 +0200

    #35 #38 Making sure that checkout respects the branch on first and subsequent uses. Making sure the MarkdownWriter base constructor is called.

--- JustReleaseNotes/writers/GithubMarkdownWriter.py
+++ JustReleaseNotes/writers/GithubMarkdownWriter.py
@@ -3,7 +3,7 @@ from JustReleaseNotes.writers import MarkdownWriter
 class GithubMarkdownWriter(MarkdownWriter.MarkdownWriter):
 
     def __init__(self, ticketProvider):
-        super(GithubMarkdownWriter, self).__init__(ticketProvider)
+        MarkdownWriter.MarkdownWriter.__init__(self, ticketProvider)
 
     def getImageBlock(self, icon):
         return "<img src=\"{0}\" width=16 height=16></img>".format(icon)
--- JustReleaseNotes/writers/StashMarkdownWriter.py
+++ JustReleaseNotes/writers/StashMarkdownWriter.py
@@ -3,7 +3,7 @@ from JustReleaseNotes.writers import MarkdownWriter
 class StashMarkdownWriter(MarkdownWriter.MarkdownWriter):
 
     def __init__(self, ticketProvider):
-        super(StashMarkdownWriter, self).__init__(ticketProvider)
+        MarkdownWriter.MarkdownWriter.__init__(self, ticketProvider)
 
     def getImageBlock(self, icon):
         return ""
