8b4768fc4a5c21bce1e98122a7757fbd560c66e9
Rafał Nowosielski <rafal@nowosielski.link>
2015-09-28 07:56:46 +0200

    Add tests for GitRepo

--- tests/sourcers/GitRepo_Test.py
+++ tests/sourcers/GitRepo_Test.py
@@ -2,7 +2,65 @@ import unittest
 from JustReleaseNotes.sourcers import GitRepo
 from mock import Mock, MagicMock, mock_open, patch
 
+class Commit:
+
+    def __init__(self, hexsha):
+        self.hexsha = hexsha
+        self.parents = []
+        self.summary = ""
+        self.message = ""
+        self.authored_date = ""
+
 class GitRepo_Test(unittest.TestCase):
+
+    @patch("git.Repo.clone_from")
+    @patch("git.Repo.iter_commits")
+    def test_retrieveHistoryProcessesEachCommit(self, iter_commits, repo_mock):
+        commit1 = Commit("1")
+        commit2 = Commit("2")
+        commit3 = Commit("3")
+        iter_commits.return_value = [commit1, commit3, commit2]
+        mock = MagicMock()
+        mock.iter_commits = iter_commits
+        repo_mock.return_value = mock
+        conf = { "Directory" : "testDir", "RepositoryUrl" : "git://some.url/repo.git" }
+        repo = GitRepo.GitRepo(conf)
+        repo.checkout()
+        repo.retrieveHistory()
+        self.assertIn("1", repo.gitCommitMessagesByHash)
+        self.assertIn("1", repo.gitCommitsList)
+        self.assertIn("1", repo.gitDatesByHash)
+        self.assertIn("2", repo.gitCommitMessagesByHash)
+        self.assertIn("2", repo.gitCommitsList)
+        self.assertIn("2", repo.gitDatesByHash)
+        self.assertIn("3", repo.gitCommitMessagesByHash)
+        self.assertIn("3", repo.gitCommitsList)
+        self.assertIn("3", repo.gitDatesByHash)
+
+    def test_setParentsTraersesChildrenAndDelasWithCycles(self):
+        conf = { "Directory" : "testDir", "RepositoryUrl" : "git://some.url/repo.git" }
+        repo = GitRepo.GitRepo(conf)
+        commit = Commit("32423424af")
+        commit.message = "test message"
+        commit.summary = "some summary"
+        commit.authored_date = ""
+        commit.parents = [ ]
+        repo.processCommit(commit)
+        self.assertIn("32423424af", repo.gitCommitMessagesByHash)
+        self.assertIn("32423424af", repo.gitCommitsList)
+        self.assertIn("32423424af", repo.gitDatesByHash)
+
+    def test_setParentsTraersesChildrenAndDelasWithCycles(self):
+        conf = { "Directory" : "testDir", "RepositoryUrl" : "git://some.url/repo.git" }
+        repo = GitRepo.GitRepo(conf)
+        commit = Commit("32423424af")
+        child = Commit("1")
+        child.parents = [commit]
+        commit.parents = [ child, Commit("2") ]
+        repo.setParents(commit)
+        self.assertEqual(2, len(repo.commitParents["32423424af"]))
+        self.assertIn("32423424af", repo.commitParents)
+
 
     @patch("git.Repo.clone_from")
     @patch("os.path.isdir")
