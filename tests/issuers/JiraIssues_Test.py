9e0b35620778a6027beeeabcbbe00f801d71b3eb
Marian Maruniak <mmaruniak@cimpress.com>
2015-10-08 11:38:42 +0200

    #47 Handle Unicode names in ticket reporters properly

--- tests/issuers/JiraIssues_Test.py
+++ tests/issuers/JiraIssues_Test.py
@@ -7,16 +7,20 @@ class JiraIssues_Test(unittest.TestCase):
 
   def test_retrieveIssueInfoWhenNoCachedData_ConnectsToTheSource(self):
     requests.packages.urllib3.disable_warnings()
-    ticketResponse = '{ "html_url" : "https://api.github.com/repos/cimpress-mcp/PostalCodes.Net/issues/23", ' \
+    ticketResponse = bytearray('{ "html_url" : "https://api.github.com/repos/cimpress-mcp/PostalCodes.Net/issues/23", ' \
                      '"title" : "Ticket\'s title",' \
                      '"fields" : {' \
                      '"summary" : "Title of TCKT-23",' \
                      '"status" : { "name" : "New", "iconUrl" : "http://site.com/imageUrl.png" },' \
                      '"issuetype" : { "name" : "Task", "iconUrl" : "http://site.com/imageUrl.png" },'\
                      '"priority" : { "name" : "Critical", "iconUrl" : "http://site.com/imageUrl.png" },'\
-                     '"reporter" : { "displayName" : "Test User" }'\
-                     ' }' \
-                     ' }'
+                     '"reporter" : { "displayName" : "', 'utf-8')
+
+    # add an unicode character
+    ticketResponse.append(0xc4)
+    ticketResponse.append(0xb1)
+    for x in bytearray('" } } }', 'utf-8'):
+        ticketResponse.append(x)
 
     config = { "Provider" : "JiraIssues",
                "HtmlUrl" : "https://jira.com/issues",
@@ -24,7 +28,7 @@ class JiraIssues_Test(unittest.TestCase):
                "Url" : "https://jira.com/rest/api/2/issues" }
 
     with requests_mock.mock() as m:
-        m.get('https://jira.com/rest/api/2/issues/TCKT-23', text=ticketResponse)
+        m.get('https://jira.com/rest/api/2/issues/TCKT-23', text=ticketResponse.decode('utf-8'))
         issuer = JiraIssues.JiraIssues(config);
 
         ticketInfo = issuer.getTicketInfo("TCKT-23")
